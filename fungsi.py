ec41224f3e96636a23c9938623bc605900ebc697
yusufalharits <yusufalharits2515@gmail.com>
2022-05-27 15:51:16 +0700

    Initial release

---
+++ fungsi.py
@@ -0,0 +1,14 @@
+print('haloo')
+
+def selamat_datang():
+    print('haloo selamat datang')
+
+selamat_datang()
+
+def greetings(nama, waktu):
+    print(f'hello {nama} selamat {waktu}')
+    print(f'apakah {nama} sudah datang {waktu} ini?')
+
+greetings('ilham', 'siang')
+greetings('andi', 'malam')
+greetings('dinda', 'siang')
